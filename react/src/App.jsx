46fc6e56195e23481ecfaaf9589a1a3b747986b6
Justin Bilao <96006037+justinnnb@users.noreply.github.com>
2024-06-15 02:52:46 +1000

    testing token

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,11 +5,20 @@ import './App.css'
 import UploadReceipt from './components/UploadReceipt'
 
 function App() {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+      const queryParams = new URLSearchParams(window.location.search);
+      const token = queryParams.get('token');
+      setToken(token);
+  }, []);
+  console.log('test')
 
   return (
     <>
       <div className="App">
       <h1>Payment System</h1>
+      {token ? <h2>Token: {token}</h2> : <h1>No Token</h1>}
       <UploadReceipt />
     </div>
     </>
